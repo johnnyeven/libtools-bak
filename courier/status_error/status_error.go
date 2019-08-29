6f6a9df426e65e091d50d3cc48ea0f8b98cf61a1
liyiwen <liyiwen@huoyunren.com>
2019-08-29 21:01:16 +0800

    status_error

--- courier/status_error/status_error.go
+++ courier/status_error/status_error.go
@@ -11,10 +11,19 @@ import (
 // code, key, msg, desc, canBeTalkError
 var RegexpStatusError = regexp.MustCompile(`@httpError\(([0-9]+),(.+),"(.+)?","(.+)?",(false|true)\);`)
 
+const DefaultErrorCode = 500000000
+const DefaultErrorKey = "InternalError"
+
 func ParseString(s string) *StatusError {
 	matched := RegexpStatusError.FindStringSubmatch(s)
 	if len(matched) != 6 {
-		return nil
+		return &StatusError{
+			Code:           DefaultErrorCode,
+			Key:            DefaultErrorKey,
+			Msg:            "",
+			Desc:           s,
+			CanBeErrorTalk: false,
+		}
 	}
 
 	code, _ := strconv.ParseInt(matched[1], 10, 64)
